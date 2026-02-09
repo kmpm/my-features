2456ad8fbc57a1a00e67ba5d5d11f3bdd8e2c518
kmpm <me@kmpm.se>
2026-02-09 15:44:10 +0000

    fix<mssql-odbc>: ubuntu install works

--- test/mssql-odbc/test.sh
+++ test/mssql-odbc/test.sh
@@ -6,6 +6,8 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
+# check "step1" dpkg -p msodbcsql18
+# check "opt_microsoft" test -d "/opt/microsoft"
 check "msodbcsql_version" odbcinst -j 
 # check "msodbcsql_location" bash -c "odbcinst -q -d | grep 'msodbcsql18'"
 
